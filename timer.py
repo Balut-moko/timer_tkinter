1aba0ef6811f2bda90666d4ae2d7a1629d8fa2b9
Balut-moko <hiro.account@gmail.com>
2020-10-08 17:25:43 +0900

    config 00:00 to 99:59

--- timer.py
+++ timer.py
@@ -1,7 +1,8 @@
 '''
-timerApp
+timerApp 00:00 to 99:59
 '''
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
 class Timer(tk.Frame):
@@ -11,6 +12,7 @@ class Timer(tk.Frame):
         self.timer_config()
         self.create_widgets()
         self.pack()
+        self.countdown()
 
     def timer_config(self):
         self.state = False
@@ -20,14 +22,28 @@ class Timer(tk.Frame):
         self.secs = 0
 
     def create_widgets(self):
+        self.spinbox_min = ttk.Spinbox(self, from_=0, to=99, command=self.spin_changed)
+        self.spinbox_min.set(self.minutes)
+        self.spinbox_min.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
+        self.spinbox_sec = ttk.Spinbox(self, from_=0, to=59, wrap=True, command=self.spin_changed)
+        self.spinbox_sec.set(self.seconds)
+        self.spinbox_sec.grid(row=0, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
         self.tokei = tk.Label(self, font=('Helvetica', 100), textvariable="")
         self.tokei.config(text="00:00")
-        self.tokei.grid(row=0, column=0, columnspan=2)
-        self.start_button = tk.Button(self, bg="Green", activebackground="Dark Green", text="開始", width=8, height=4, command=self.start)
-        self.start_button.grid(row=1, column=0)
-        self.pause_button = tk.Button(self, bg="Red", activebackground="Dark Red", width=8, height=4, command=self.pause)
-        self.pause_button.config(text="一時停止")
-        self.pause_button.grid(row=1, column=1)
+        self.tokei.grid(row=1, column=0, columnspan=3)
+        self.start_button = tk.Button(self, bg="Red", activebackground="Dark Red", font=('Helvetica', 20), command=self.start)
+        self.start_button.config(text="Start")
+        self.start_button.grid(row=2, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
+        self.pause_button = tk.Button(self, bg="Green", activebackground="Dark Green", font=('Helvetica', 20), command=self.pause)
+        self.pause_button.config(text="Pause")
+        self.pause_button.grid(row=2, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+        self.reset_button = tk.Button(self, bg="Blue", activebackground="Dark Blue", font=('Helvetica', 20), command=self.reset)
+        self.reset_button.config(text="Reset")
+        self.reset_button.grid(row=2, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+
+    def spin_changed(self):
+        self.minutes = int(self.spinbox_min.get())
+        self.seconds = int(self.spinbox_sec.get())
 
     def countdown(self):
         if self.state:
@@ -35,16 +51,17 @@ class Timer(tk.Frame):
 
             if (self.mins == 0) and (self.secs == 0):
                 self.tokei.config(text="Done!")
+                self.state = False
             else:
                 if self.secs == 0:
                     self.mins -= 1
                     self.secs = 59
                 else:
                     self.secs -= 1
-
                 self.master.after(1000, self.countdown)
 
-        elif not self.state:
+        else:
+            self.tokei.config(text="{}:{}".format(str(self.mins).zfill(2), str(self.secs).zfill(2)))
             self.master.after(100, self.countdown)
 
     def start(self):
@@ -52,11 +69,21 @@ class Timer(tk.Frame):
             self.state = True
             self.mins = self.minutes
             self.secs = self.seconds
-            self.countdown()
+            self.start_button.config(state=tk.DISABLED)
 
     def pause(self):
         if self.state:
             self.state = False
+            self.pause_button.config(text="Resume")
+            self.start_button.config(state=tk.NORMAL)
+
+        else:
+            self.state = True
+            self.pause_button.config(text="Pause")
+
+    def reset(self):
+        self.mins = self.minutes
+        self.secs = self.seconds
 
 
 if __name__ == "__main__":
